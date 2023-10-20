cf6e14960f24cda3b08d2c364cc2db24a0de0769
alberto-david <albertodavid254@gmail.com>
2023-10-20 20:26:08 +0300

    portfolio

---
+++ WOLF_AI_img-main/dalle_generator/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import openai
+
+def generate_image(request):
+    api_key = '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'
+    prompt = request.GET.get('prompt', 'Generate an image')
+    size = request.GET.get('size', '512x512')
+    num_images = 4
+
+    openai.api_key = api_key
+
+    try:
+        response = openai.Image.create(
+            prompt=prompt,
+            n=num_images,
+            size=size
+        )
+
+        images = []
+        for image_data in response['data']:
+            image_url = image_data['url']
+            images.append(image_url)
+
+        return render(request, 'dalle_generator/generate_image.html', {'images': images})
+
+    except Exception as e:
+        error_message = str(e)
+        return render(request, 'dalle_generator/error.html', {'error_message': error_message})
